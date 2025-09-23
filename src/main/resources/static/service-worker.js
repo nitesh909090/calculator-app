dc4f94ad8fd15a84cbd854fdf32214feb855b169
nitesh909090 <csecse857@gmail.com>
2025-09-23 12:46:04 +0530

    Update service-worker.js

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -2,13 +2,31 @@ const CACHE_NAME = "calculator-cache-v1";
 const urlsToCache = [
   "/",
   "/manifest.json",
-  "/service-worker.js"
+  "/service-worker.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/style.css",
+  "/app.js"
 ];
 
 // Install event
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+// Activate event (clear old cache)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
   );
 });
 
